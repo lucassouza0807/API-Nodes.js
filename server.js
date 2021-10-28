aa7c73cfb34fb49a0a0c7b20d2ced8d0fcd474c2
MD12nice <lucassouza0807@gmail.com>
2021-10-28 20:54:12 -0300

    deploy

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const HTTP = require("http")
-const express = require("express")
+require("dotenv").config();
+
+const HTTP = require("http");
+const express = require("express");
 
 //Routes
-const vagas = require("./api/routes/vagas")
+const vagas = require("./api/routes/vagas");
 const app = express()
 
 //Server settings
-const HOSTNAME = "https://trabalhe-facil.herokuapp.com/" ;
-const PORT = 80 ;
 
 
 const server = HTTP.createServer( async (request, response) => {
     response.setHeader("Content-Type", "application/json");
 });
 
-app.listen(PORT, async () => {
+app.listen(process.env.PORT, async () => {
     console.log(`Server running at ${HOSTNAME}:${PORT}`);
 });
 
-app.use(vagas)
+app.use(vagas);
